eadbe8689cd3a1c6100312e3085736f1b67f5104
nathanshaw <nathanshawsemail@gmail.com>
2015-12-06 14:07:11 -0800

    added support for slower alternation between the two players, overloaded several methods to deal with both ints and bytes for allow for NES and SNES support?

--- snes_two_player_as_one_player/snes_two_player_as_one_player.ino
+++ snes_two_player_as_one_player/snes_two_player_as_one_player.ino
@@ -1,41 +1,48 @@
+/*
+    Arduino Firmware for dealing with alternating between
+    two SNES or NES controllers
+
+    Programmed by Nathan Villicana-Shaw
+    Fall 2015
+*/
 #include <SNESpad.h>
-
 // the pins we use for receiving data from the two SNES controllers
-#define STROBE1 2
-#define CLOCK1 3
-#define DATA1 4
-#define STROBE2 5
-#define CLOCK2 6
-#define DATA2 7
-#define ledPin 13  // goes on when we're sending midi data
-
+#define IN1_STROBE 2
+#define IN1_CLOCK 3
+#define IN1_DATA 4
+#define IN2_STROBE 5
+#define IN2_CLOCK 6
+#define IN2_DATA 7
 // the pins we use for sending the data to a SNES.
 #define OUT_DATA 8
 #define OUT_STROBE 9
 #define OUT_CLOCK 10
-
 // modes of operation
 #define PLAYERS_AGREE 1
 #define PLAYERS_ALTERNATE 2
-
+// number of players or controllers (might support more in future)
+#define NUM_PLAYERS 2
 // for dev and debug
-#define DEBUG true
+#define DEBUG false
 
-// TODO : create if in defines to add alternation time if players alternate
+int mode = PLAYERS_ALTERNATE;
 
+long pastPoll;
+int turnLength = 500;
 
-// create object for each SNES controller
-SNESpad snes1 = SNESpad(STROBE1, CLOCK1, DATA1);
-SNESpad snes2 = SNESpad(STROBE2, CLOCK2, DATA2);
+int activePlayer = 0;
+long turnStart;
 
-int state1; // joystick button state
-int state2;
-
-int mode = PLAYERS_AGREE;
+byte state1;
+byte state2;
+byte state;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(38400);
+    // put your setup code here, to run once:
+    Serial.begin(57600);
+    // create object for each SNES controller
+    SNESpad snes1 = SNESpad(IN1_STROBE, IN1_CLOCK, IN1_DATA);
+    SNESpad snes2 = SNESpad(IN2_STROBE, IN2_CLOCK, IN2_DATA);
 }
 
 
@@ -43,30 +50,43 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (mode == PLAYERS_ALTERNATE) {
     playersAlternate();
-    }
+  }
   else if (mode == PLAYERS_AGREE) {
     playersAgree();
-    }
+  }
 }
+
 
 void playersAgree() {
   state1 = snes1.buttons();
   state2 = snes2.buttons();
-  int agreed_state = state1 & state2;
+  state = snes1.buttons() & state2;
   sendToSnes(agreed_state);
-  printBits(state1);
-  Serial.print("-");
-  printBits(state2);
-  Serial.print("||");
-  printBits(agreed_state);
-  Serial.println();
+  if (DEBUG = true){
+      printBits(state1);
+      Serial.print("-");
+      printBits(state2);
+      Serial.print("||");
+      printBits(agreed_state);
+      Serial.println();
+    }
 }
 
+
 void playersAlternate() {
-  state1 = snes1.buttons(); 
-  sendToSnes(state1);
-  state2 = snes2.buttons();
-  sendToSnes(state2);
+    // if the current turn is over
+    // move to the next turn
+    if (millis() > turnStart + TURN_LENGTH){
+        activePlayer = (activePlayer + 1) % NUM_PLAYERS;
+        turnStart = millis();
+    }
+  
+  if (activePlayer == 1){
+    state = snes1.buttons(); 
+  }
+  else if (activePlayer == 2) {
+    state = snes2.buttons(); 
+  }
   if (DEBUG == true){
     printControllerStates(state1, state2);
   }
@@ -76,16 +96,29 @@ void printControllerStates(int state1, int state2) {
   printBits(state1);
   Serial.print("-");
   printBits(state2);
-  delayMicroseconds(500);
-  Serial.println();
+  Serial.println(" ");
+}
+// overloaded to account for bytes and ints
+void printControllerStates(byte state1, byte state2) {
+    printBits(state1);
+    Serial.print("-");
+    printBits(state2);
+    Serial.println(" ");
 }
 
-void sendToSnes(int message) {
-  // send the message out to the SNES
-}
+// ------------ Printing and DeBugging ----------------
 
+//overloaded to handle bytes and ints
 void printBits(int myByte){
  for(int mask = 0x8000; mask; mask >>= 1){
+   if(mask  & myByte)
+       Serial.print('1');
+   else
+       Serial.print('0');
+ }
+}
+ void printBits(byte myByte) {
+ for(byte mask = 0x80; mask; mask >>= 1){
    if(mask  & myByte)
        Serial.print('1');
    else
